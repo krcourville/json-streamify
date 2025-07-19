53d1016d739d98000ec8dadac271476adb52a83e
Ken Courville <krcourville@users.noreply.github.com>
2025-07-19 13:56:29 -0600

    fix esm support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Re-export all functions from their individual modules
-export { jsonStreamify } from './json-streamify';
-export { streamToString } from './stream-to-string';
-export { createStreamFromFile } from './create-stream-from-file';
+export { jsonStreamify } from './json-streamify.js';
+export { streamToString } from './stream-to-string.js';
+export { createStreamFromFile } from './create-stream-from-file.js';
 
 // Default export
-export { jsonStreamify as default } from './json-streamify';
+export { jsonStreamify as default } from './json-streamify.js';
